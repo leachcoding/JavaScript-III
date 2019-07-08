8df054fedc1a5b683472e34e863d999a8cce587d
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-07-08 19:09:29 -0400

    Near Finished with this.js / Starting stretch for prototypes.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding: When in the global scope, the value of “this” will be the window/console Object.
+
+* 2. Implicit Binding: Whenever a function is called by a preceding dot, the object before that dot is this.
+
+* 3. New Binding: A constructor function is a function that returns an object. It’s an object creator. We use them a lot in JavaScript and they lend themselves to a paradigm called: ‘Object Oriented Programming’. CordialPerson will be a function that creates an object for us. When we call it we have to use the new keyword.
+
+* 4. Explicit Binding: Whenever JavaScript’s call or apply method is used, this is explicitly defined.
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +15,13 @@
 // Principle 1
 
 // code example for Window Binding
+
+function myName(name) {
+  console.log(this); //This will set value of this as the window/console Object
+  return name;
+}
+
+myName("Jay Leach");
 
 // Principle 2
 
